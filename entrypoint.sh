15515083801a44380e7110375e4d1c031ae1f4c2
Shivam Khandelwal <shivam.khandelwal@maf.ae>
2024-04-05 14:32:51 +0530

    Comments added

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,12 @@
 #! /bin/sh
 
+# To fix error "fatal: detected dubious ownership in repository at '/src'"
+git config --global --add safe.directory /src
+
+# Changing dir to generate the diagram in the same directory
 cd -P "$( dirname "$1" )"
 python "$( basename "$1" )"
+
+# Might add any other png files in the directory as well not intended for commit. 
+# Other workaround is to git stash first then run the python generator and do this and then pop.
 ls *.png | xargs git add
